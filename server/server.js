b6ff170f60fcf345a04c61386a4ad3131ab7499b
liam02walker <17liamwalker02@gmail.com>
2023-11-02 09:58:31 +0000

    init bruv

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+
+// add your endpoints here
+app.get(PORT, (_, response) => response.json("Root route for translatim."));
+
+// Last line of code
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
